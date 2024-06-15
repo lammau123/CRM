baa871300f89e6627ad71fc1b30cd81a2cd4da85
Mau Lam Tuan <lam6799@saskpolytech.ca>
2024-06-14 23:45:23 -0600

    Update

--- crm/webapp/models.py
+++ crm/webapp/models.py
@@ -26,14 +26,14 @@ class ContactDto:
         
     def to_dict(self):
         return {
+            "id": self.id,
             "last_name": self.last_name, 
             "first_name": self.first_name, 
             "email": self.email, 
             "phone": self.phone, 
             "company": self.company,
             "created_at": self.created_at,
-            "updated_at": self.updated_at,
-            "id": self.id
+            "updated_at": self.updated_at
         }
         
     @staticmethod
@@ -43,48 +43,6 @@ class ContactDto:
     @staticmethod
     def get_field_names():
         return ['first_name last_name', 'company', 'email', 'phone', 'created_at', 'updated_at']
-
-@dataclass(frozen=True)
-class TaskTypeDto:
-    name: str
-    
-    def __str__(self):
-        return self.name
-
-    def toDict(self):
-        return { "name": self.name }
-
-@dataclass(frozen=True)
-class TaskStatusDto:
-    name: str
-       
-    def __str__(self):
-        return self.name
-
-    def to_dict(self):
-        return { "name": self.name }
-
-@dataclass(frozen=True)
-class TaskDto:
-    id: int
-    title: str
-    opportunity_id: int
-    due_date: datetime
-    type_id: int
-    status_id: int
-
-    def __str__(self):
-        return self.title
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "title": self.title, 
-            "opportunity": self.opportunity_id, 
-            "due_date": self.due_date, 
-            "type_id": self.type_id, 
-            "status_id": self.status_id 
-        }
 
 @dataclass(frozen=True)
 class OpportunityStatusDto:
@@ -145,22 +103,102 @@ class OpportunityDto:
     
     def update(self, form):
         data = self.to_dict()
-        
         for field in form.changed_data:
             data[field] = form.cleaned_data[field]
-            
         return OpportunityDto(**data)
     
-    def to_dict(self):
+    def to_ref_dict(self):
         return {
+            "id": self.id,
             "name": self.name, 
             "amount": self.amount, 
             "user": self.user.id, 
             "contact": self.contact.id, 
             "status": self.status.id,
             "opened_at": self.opened_at,
-            "closed_at": self.closed_at,
-            "id": self.id
+            "closed_at": self.closed_at
+        }
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name, 
+            "amount": self.amount, 
+            "user": self.user.to_dict(), 
+            "contact": self.contact.to_dict(),
+            "status": self.status.to_dict(),
+            "opened_at": self.opened_at,
+            "closed_at": self.closed_at
         }
 
+@dataclass(frozen=True)
+class TaskTypeDto:
+    id: int
+    name: str
+    
+    def __str__(self):
+        return self.name
+
+    def to_dict(self):
+        return { "id": self.id, "name": self.name }
+
+@dataclass(frozen=True)
+class TaskStatusDto:
+    id: int
+    name: str
+       
+    def __str__(self):
+        return self.name
+
+    def to_dict(self):
+        return { "id": self.id, "name": self.name }
+
+@dataclass(frozen=True)
+class TaskDto:
+    id: int
+    title: str
+    opportunity: OpportunityDto
+    due_date: datetime
+    type: TaskTypeDto
+    status: TaskStatusDto
+
+    def __init__(self, id, title, opportunity, due_date, type, status):
+        object.__setattr__(self, 'id', id)
+        object.__setattr__(self, 'title', title)
+        object.__setattr__(self, 'opportunity', OpportunityDto(**opportunity))
+        object.__setattr__(self, 'due_date', due_date)
+        object.__setattr__(self, 'type', TaskTypeDto(**type))
+        object.__setattr__(self, 'status', TaskStatusDto(**status))
+        
+    def __str__(self):
+        return self.title
+
+    def to_ref_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title, 
+            "opportunity": self.opportunity_id, 
+            "due_date": self.due_date, 
+            "type_id": self.type_id, 
+            "status_id": self.status_id 
+        }
+        
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title, 
+            "opportunity": self.opportunity.to_dict(), 
+            "due_date": self.due_date, 
+            "type": self.type.to_dict(), 
+            "status": self.status.to_dict() 
+        }
+
+    @staticmethod
+    def get_header_names():
+        return ['Title', 'Opportunity', 'Due Date', 'Type', 'Status']
+    
+    @staticmethod
+    def get_field_names():
+        return ['title', 'opportunity', 'due_date', 'type', 'status']
+    
 # https://github.com/radzenhq/radzen-examples/blob/master/CRMDemo/crm-database-schema.sql
--- crm/webapp/repository/repositories.py
+++ crm/webapp/repository/repositories.py
@@ -1,4 +1,4 @@
-from ..models import ContactDto, OpportunityDto, UserDto, OpportunityStatusDto
+from ..models import ContactDto, OpportunityDto, UserDto, OpportunityStatusDto, TaskDto, TaskTypeDto, TaskStatusDto
 from datetime import datetime
 
 list_of_users = [
@@ -24,8 +24,6 @@ list_of_contacts = [
         ContactDto(id=1, first_name='first', last_name='last', phone='123-123-123', company='123 ooo st', email="asd@gmail.com", created_at=datetime(2024, 5, 21, 9, 0, 0), updated_at=datetime(2024, 5, 21, 9, 0, 0)),
         ContactDto(id=1, first_name='first', last_name='last', phone='123-123-123', company='123 ooo st', email="asd@gmail.com", created_at=datetime(2024, 5, 21, 9, 0, 0), updated_at=datetime(2024, 5, 21, 9, 0, 0)),
     ]
-
-
 
 list_of_opportunities = [
         OpportunityDto(
@@ -75,15 +73,36 @@ list_of_opportunities = [
             closed_at=datetime(2024, 5, 21, 9, 0, 0)),
 ]
 
-list_of_tasks = [
-]
-
 list_of_opportunity_status = [
     OpportunityStatusDto(1, 'New'),
     OpportunityStatusDto(2, 'Inprogress'),
     OpportunityStatusDto(3, 'Delay'),
     OpportunityStatusDto(4, 'Cancel'),
     OpportunityStatusDto(5, 'Completed'),
+]
+
+list_of_task_type = [
+    TaskTypeDto(id=1, name='task type1'),
+    TaskTypeDto(id=2, name='task type2'),
+    TaskTypeDto(id=3, name='task type3'),
+    TaskTypeDto(id=4, name='task type4'),
+    TaskTypeDto(id=5, name='task type5'),
+]
+
+list_of_task_status = [
+    TaskStatusDto(id=1, name='task status1'),
+    TaskStatusDto(id=2, name='task status2'),
+    TaskStatusDto(id=3, name='task status3'),
+    TaskStatusDto(id=4, name='task status4'),
+    TaskStatusDto(id=5, name='task status5'),
+]
+
+list_of_tasks = [
+    TaskDto(id=1, title='task1', opportunity=list_of_opportunities[0].to_dict(), due_date=datetime.now(), type=list_of_task_type[0].to_dict(), status=list_of_task_status[0].to_dict()),
+    TaskDto(id=2, title='task2', opportunity=list_of_opportunities[1].to_dict(), due_date=datetime.now(), type=list_of_task_type[1].to_dict(), status=list_of_task_status[1].to_dict()),
+    TaskDto(id=3, title='task3', opportunity=list_of_opportunities[2].to_dict(), due_date=datetime.now(), type=list_of_task_type[2].to_dict(), status=list_of_task_status[2].to_dict()),
+    TaskDto(id=4, title='task4', opportunity=list_of_opportunities[3].to_dict(), due_date=datetime.now(), type=list_of_task_type[3].to_dict(), status=list_of_task_status[3].to_dict()),
+    TaskDto(id=5, title='task5', opportunity=list_of_opportunities[4].to_dict(), due_date=datetime.now(), type=list_of_task_type[4].to_dict(), status=list_of_task_status[4].to_dict()),
 ]
 
 async def get_contacts():
@@ -116,3 +135,17 @@ async def get_contact_by_id(id):
 async def get_opportunity_by_id(id):
     opportunity_by_id = { opportunity.id: opportunity for opportunity in await get_opportunities() }
     return opportunity_by_id[id]
+
+async def get_task_types():
+    return list_of_task_type
+
+async def get_task_type_by_id(id):
+    type_by_id = { type.id: type for type in await get_task_types() }
+    return type_by_id[id]
+
+async def get_task_statuses():
+    return list_of_task_status
+
+async def get_task_status_by_id(id):
+    status_by_id = { status.id: status for status in await get_task_statuses() }
+    return status_by_id[id]
